03d6babd1029d519efc682b08e166d0c4f3d3f05
Lukas Freitas de Carvalho <lukascarvalho712@gmail.com>
2024-02-06 13:39:02 -0300

    Mudança no path

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from PyPDF2 import PdfWriter
 import os
 
 merge = PdfWriter()
-archive = os.listdir("/Users/Lukas/Vscode/Mesclador-de-PDF-s/arquivos")
+archive = os.listdir("arquivos")
+print(archive)
 
 for pdf in archive:
-    merge.append(f"/Users/Lukas/Vscode/Mesclador-de-PDF-s/arquivos/{pdf}")
+    merge.append(f"arquivos/{pdf}")
 
 merge.write("Arquivos-Juntos.pdf")
 merge.close()
